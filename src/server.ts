62b6ede6b6c06872f8868d3ea39dc0547d08f240
Roberto Sousa <robertosousa186@gmail.com>
2021-11-14 20:41:20 -0300

    Creating route 'users' for create a user

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
+import cors from 'cors';
 import express from 'express';
+import * as user from './controllers/users';
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (request, response) => {
   return response.send('Hello!');
 });
+
+server.post('/users', user.create);
 
 export default server;
