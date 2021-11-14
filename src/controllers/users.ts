f87bca223a0091e55c3d548a4075e9234f6c74ac
Roberto Sousa <robertosousa186@gmail.com>
2021-11-14 20:40:27 -0300

    Creating 'create' function

---
+++ src/controllers/users.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import * as user from '../services/users';
+import { error } from '../libs/bindError';
+
+const create = async (req: Request<any>, res: Response<any>) => {
+  try {
+    const { email, password } = req.body;
+
+    await user.create({ email, password });
+
+    return res
+      .status(201)
+      .json({ message: 'Usuário(a) criado(a) com sucesso!' });
+  } catch (err: any) {
+    return error(res, err);
+  }
+};
+
+export { create };
